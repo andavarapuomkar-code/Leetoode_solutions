b3e867c7296c6a2f75c05d4d89564277aec4f20b
Andavarapu Omkar <andavarapuomkar@gmail.com>
2026-09-01 17:56:30 +0530

    Time: 191 ms (45.50%) | Memory: 25.9 MB (64.02%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        cnt = {}
+        left = ans = 0
+        for right in range(len(fruits)):
+            cnt[fruits[right]] = cnt.get(fruits[right], 0) + 1
+            while len(cnt) > 2:
+                cnt[fruits[left]] -= 1
+                if cnt[fruits[left]] == 0:
+                    del cnt[fruits[left]]
+                left += 1
+            ans = max(ans, right - left + 1)
+        return ans
